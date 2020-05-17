3f37c1f93a97f13661f3a74a54c812543e0887b6
Collin <ococncol@gmail.com>
2020-05-17 17:44:30 -0400

    First pass at working Add Cheese form

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -4,14 +4,7 @@ from . import views
 app_name = "cheese"
 
 urlpatterns = [
-    path(
-      route='',
-      view=views.CheeseList.as_view(),
-      name='list'
-    ),
-    path(
-      route='<slug:slug>/',
-      view=views.CheeseDetailView.as_view(),
-      name='detail',
-    )
+    path(route="", view=views.CheeseList.as_view(), name="list"),
+    path(route="add/", view=views.CheeseCreateView.as_view(), name="add",),
+    path(route="<slug:slug>/", view=views.CheeseDetailView.as_view(), name="detail",),
 ]
